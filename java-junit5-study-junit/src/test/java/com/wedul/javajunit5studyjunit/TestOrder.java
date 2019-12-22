a9fe2e917df83a2d25c1dc4c636243293ed8ab8f
we dul <wedul.chul@gmail.com>
2019-12-23 00:49:01 +0900

    junit-flatform properties 지정

--- java-junit5-study-junit/src/test/java/com/wedul/javajunit5studyjunit/TestOrder.java
+++ java-junit5-study-junit/src/test/java/com/wedul/javajunit5studyjunit/TestOrder.java
@@ -11,24 +11,31 @@ import org.junit.jupiter.api.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestOrder {
 
+    int value = 1;
+
     @Order(1)
     @Test
     @DisplayName("테스트 순서 테스트")
     void test_order_1() {
+        System.out.println(this);
         System.out.println("1");
+        System.out.println("value : " + value++);
     }
 
     @Order(2)
     @Test
     @DisplayName("테스트 순서 테스트")
     void test_order_2() {
+        System.out.println(this);
         System.out.println("2");
+        System.out.println("value : " + value++);
     }
 
     @Order(3)
     @Test
     @DisplayName("테스트 순서 테스트")
     void test_order_3() {
+        System.out.println(this);
         System.out.println("3");
     }
 
@@ -36,6 +43,7 @@ public class TestOrder {
     @Test
     @DisplayName("테스트 순서 테스트")
     void test_order_4() {
+        System.out.println(this);
         System.out.println("4");
     }
 
