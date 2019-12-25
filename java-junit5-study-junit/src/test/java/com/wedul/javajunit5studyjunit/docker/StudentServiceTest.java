90a383134cab9ac0c2732081735c8df5eb25af74
we dul <wedul.chul@gmail.com>
2019-12-25 09:14:26 +0900

    ApplicationInitializer를 사용한 도커 value 사용하기

--- java-junit5-study-junit/src/test/java/com/wedul/javajunit5studyjunit/docker/StudentServiceTest.java
+++ java-junit5-study-junit/src/test/java/com/wedul/javajunit5studyjunit/docker/StudentServiceTest.java
@@ -3,13 +3,20 @@ package com.wedul.javajunit5studyjunit.docker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
 import javax.transaction.Transactional;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 /**
  * java-junit5-study
  *
@@ -19,18 +26,23 @@ import static org.assertj.core.api.Assertions.*;
 @ActiveProfiles("test")
 @SpringBootTest
 @Testcontainers
+@ContextConfiguration(initializers = StudentServiceTest.ContainerPropertyInitializer.class)
 class StudentServiceTest {
 
     @Autowired
     StudentService studentService;
 
     @Container
-    static MySQLContainer mariaDBContainer = new MySQLContainer();
+    static MySQLContainer mySQLContainer = new MySQLContainer();
+
+    @Value("${container.databaseName}")
+    private String databaseName;
 
     @Test
     @DisplayName("학생 추가하기")
     @Transactional
     void create_student_test() {
+        System.out.println(databaseName);
         studentService.createStudent(Student.builder()
             .studentId(2L)
             .age(10)
@@ -52,8 +64,17 @@ class StudentServiceTest {
             .studentNickname("duri")
             .build()
         );
-        Student student = studentService.getStudent(2L);
+        Student student = studentService.getStudent(1L);
         assertThat(student).isNotNull();
+    }
+
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of("container.databaseName=" + mySQLContainer.getDatabaseName())
+                .applyTo(applicationContext.getEnvironment());
+        }
     }
 
 }
